5406a65530e4ad96fad5d8b8fb8a16c8d3db5d59
kreativcare <109919243+kreativcare@users.noreply.github.com>
2022-10-31 13:02:09 +0100

    added footer

--- src/index.js
+++ src/index.js
@@ -8,6 +8,12 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <div className="footer">
+      <a href="https://github.com/kreativcare/weather-react">
+        Open-source code
+      </a>{" "}
+      by Stefanie Jahn
+    </div>
   </React.StrictMode>
 );
 
